6c7adaabd2b5306d55a21663daf4afec0bac9e7f
Andrii Sukhov <sukhov.andrii@gmail.com>
2023-05-21 19:53:33 +0200

    Update custom_table.py

--- src/views/widgets/custom_table.py
+++ src/views/widgets/custom_table.py
@@ -3,24 +3,28 @@ import tkinter.ttk as ttk
 
 
 class CustomTable(ctk.CTkFrame):
-    def __init__(self, parent, headings: list[str], column_widths: list[int], height: int = 20):
+    def __init__(self, parent, column_names: list[str], column_widths: list[int], height: int = 20):
         super().__init__(parent)
 
-        self.treeview = ttk.Treeview(self, columns=headings, height=height)
+        # Create Treeview widget for the table
+        self.treeview = ttk.Treeview(self, columns=column_names, height=height)
         self.scrollbar = ctk.CTkScrollbar(self, width=20, command=self.treeview.yview)
         self.treeview.configure(yscrollcommand=self.scrollbar.set)
         self.treeview.grid(row=0, column=0, sticky="nsew")
         self.scrollbar.grid(row=0, column=1, sticky="ns")
 
+        # Configure column properties
         self.treeview.column("#0", width=0, stretch=False)
-        for i, heading in enumerate(headings):
+        for i, heading in enumerate(column_names):
             width = column_widths[i] if column_widths else None
             self.treeview.heading(heading, text=heading.title(), anchor="center")
             self.treeview.column(heading, minwidth=width, width=width, anchor="center")
 
+        # Configure grid weights
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
 
+        # Apply custom ttk styles
         self.set_ttk_styles()
 
     def insert(self, parent, index, values):
@@ -32,7 +36,7 @@ class CustomTable(ctk.CTkFrame):
     def get_children(self, item):
         return self.treeview.get_children(item)
 
-    def selection(self):
+    def get_selection(self):
         return self.treeview.selection()
 
     def insert_rows(self, rows):
@@ -45,10 +49,12 @@ class CustomTable(ctk.CTkFrame):
 
     @staticmethod
     def set_ttk_styles():
+        """Set custom ttk styles based on appearance mode."""
         style = ttk.Style()
         style.theme_use("default")
 
-        if ctk.get_appearance_mode() == 'Light':
+        appearance_mode = ctk.get_appearance_mode()
+        if appearance_mode == 'Light':
             style.configure("Treeview",
                             background="white",
                             foreground="black",
@@ -65,7 +71,7 @@ class CustomTable(ctk.CTkFrame):
                             relief="flat",
                             font=('Helvetica', 20, 'bold'))
             style.map("Treeview.Heading", background=[('active', '#3484F0')])
-        elif ctk.get_appearance_mode() == 'Dark':
+        elif appearance_mode == 'Dark':
             style.configure("Treeview",
                             background="#2a2d2e",
                             foreground="white",
