3e3da6d8f0b5ebfbf7d4e39f6f5dc68fa4ae1820
Jared-Willemsen <jared@tommasi.nl>
2023-05-23 11:16:47 +0200

    clients page has table and searchbar

--- src/controllers/main.py
+++ src/controllers/main.py
@@ -1,6 +1,7 @@
 from .login import LoginController
 from .sidebar import SidebarController
 from .books import BooksController
+from .clients import ClientsController
 
 
 class Controller:
@@ -15,6 +16,7 @@ class Controller:
         self.signin_controller = LoginController(model, view)
         self.sidebar_controller = SidebarController(model, view)
         self.books_controller = BooksController(model, view)
+        self.clients_controller = ClientsController(model, view)
 
     def start(self):
         self.view.start_mainloop()
--- src/views/books.py
+++ src/views/books.py
@@ -3,7 +3,6 @@ import customtkinter as ctk
 from .widgets.searchbar import SearchBar
 from .widgets.custom_table import CustomTable
 
-
 class BooksView(ctk.CTkFrame):
     def __init__(self, master):
         super().__init__(master, fg_color='transparent')
@@ -16,5 +15,5 @@ class BooksView(ctk.CTkFrame):
         column_names = ['title', 'author', 'genre', 'language']
         self.search_bar = SearchBar(self, column_names, 35, 700)
         self.search_bar.grid(row=1, column=0, padx=20, pady=20)
-        self.table = CustomTable(self, column_names, [400, 400, 300, 300])
+        self.table = CustomTable(self, column_names, [600, 600, 600, 600])
         self.table.grid(row=2, column=0, padx=20, pady=20)
