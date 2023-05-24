8c43c5aa54a984c90f867ec8f9b81d79842288e5
Andrii Sukhov <sukhov.andrii@gmail.com>
2023-05-24 23:27:54 +0200

    Add database pooling and session management

--- src/models/database.py
+++ src/models/database.py
@@ -1,12 +1,13 @@
 import mysql.connector
-from mysql.connector import errorcode
+from mysql.connector import pooling
 import os
 import dotenv
+import logging
 
 
 class Database:
     """
-    A class to perform connection to a database.
+    A class to perform connection to a database using connection pooling and session management.
     """
 
     def __init__(self):
@@ -16,41 +17,92 @@ class Database:
         self.password = os.getenv("MYSQL_PASSWORD")
         self.database = os.getenv("MYSQL_DATABASE")
 
-        self.cursor = None
-        self.conn = None
+        self.pool = None
 
-    def connect(self):
-        """Establish a connection to the database"""
+    def create_connection_pool(self):
+        """Create a connection pool"""
         try:
-            self.conn = mysql.connector.connect(
+            self.pool = pooling.MySQLConnectionPool(
+                pool_name="my_pool",
+                pool_size=5,
                 host=self.host,
                 user=self.user,
                 password=self.password,
                 database=self.database
             )
-            self.cursor = self.conn.cursor()
+            logging.info("Connection pool created successfully.")
         except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print("Something is wrong with your MYSQL username or password")
-            elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                print("Database does not exist")
-            else:
-                print(f"Error: {err}")
+            logging.error(f"Error creating connection pool: {err}")
             raise
 
-    def execute_query(self, query: str, values: str = None):
-        """Execute raw SQL query"""
-        self.connect()
-        self.cursor.execute(query, values)
-        result = self.cursor.fetchall()
-        self.disconnect()
-        return result
+    def get_connection(self):
+        """Get a connection from the connection pool"""
+        if self.pool is None:
+            self.create_connection_pool()
 
-    def commit(self):
-        """Save all modifications to a database"""
-        self.conn.commit()
+        try:
+            connection = self.pool.get_connection()
+            logging.info("Connection retrieved from the connection pool.")
+            return connection
+        except mysql.connector.Error as err:
+            logging.error(f"Error getting connection from the connection pool: {err}")
+            raise
 
-    def disconnect(self):
-        """Close the cursor and connection"""
-        self.cursor.close()
-        self.conn.close()
+    @staticmethod
+    def release_connection(connection):
+        """Release the connection back to the connection pool"""
+        try:
+            connection.close()
+            logging.info("Connection released back to the connection pool.")
+        except mysql.connector.Error as err:
+            logging.error(f"Error releasing connection to the connection pool: {err}")
+            raise
+
+    def execute_query(self, query: str, values: str | tuple[str, ...] = None):
+        """Execute raw SQL query within a session"""
+        connection = self.get_connection()
+        try:
+            cursor = connection.cursor()
+            cursor.execute(query, values)
+            result = cursor.fetchall()
+            logging.info("Query executed successfully.")
+            return result
+        except mysql.connector.Error as err:
+            logging.error(f"Error executing query: {err}")
+            raise
+        finally:
+            self.release_connection(connection)
+            logging.info("Connection pool released successfully.")
+
+    def start_transaction(self):
+        """Start a new transaction"""
+        connection = self.get_connection()
+        try:
+            connection.start_transaction()
+            logging.info("Transaction started successfully.")
+            return connection
+        except mysql.connector.Error as err:
+            logging.error(f"Error starting transaction: {err}")
+            raise
+
+    def commit(self, connection):
+        """Commit the transaction"""
+        try:
+            connection.commit()
+            logging.info("Transaction committed successfully.")
+        except mysql.connector.Error as err:
+            logging.error(f"Error committing transaction: {err}")
+            raise
+        finally:
+            self.release_connection(connection)
+
+    def rollback(self, connection):
+        """Rollback the transaction"""
+        try:
+            connection.rollback()
+            logging.info("Transaction rolled back successfully.")
+        except mysql.connector.Error as err:
+            logging.error(f"Error rolling back transaction: {err}")
+            raise
+        finally:
+            self.release_connection(connection)
