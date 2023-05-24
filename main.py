f5aaa442db9f57f8c72c864a2dead0b0ddf8c744
Andrii Sukhov <sukhov.andrii@gmail.com>
2023-05-24 21:18:10 +0200

    Implement logging functionality

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+import logging
+
 from src.models.main import Model
 from src.views.main import View
 from src.controllers.main import Controller
 
 
+def configure_logging():
+    logging.basicConfig(
+        level=logging.WARNING,
+        filename='logs/app.log',
+        format='%(asctime)s | %(levelname)s | %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
+
 def main():
+    configure_logging()
+
     model = Model()
     view = View()
     controller = Controller(model, view)
