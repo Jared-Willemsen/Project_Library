478c28cbc7c7f1b69bafcb700b4fb4a1cd8a67fe
Andrii Sukhov <sukhov.andrii@gmail.com>
2023-06-06 10:56:06 +0200

    Update books.py

--- src/views/books.py
+++ src/views/books.py
@@ -1,8 +1,12 @@
 import customtkinter as ctk
+from PIL import Image
+import os
 
 from .widgets.searchbar import SearchBar
 from .widgets.custom_table import CustomTable
 from .widgets.data_form import DataForm
+from src.resources.config import IMAGES_DIR
+
 
 class BooksView(ctk.CTkFrame):
     def __init__(self, master):
@@ -10,16 +14,38 @@ class BooksView(ctk.CTkFrame):
         self.grid_columnconfigure(0, weight=1)
         column_names = ['title', 'author', 'genre', 'language']
 
+        # load images
+        self.add_image = ctk.CTkImage(Image.open(os.path.join(IMAGES_DIR, 'add_light.png')),
+                                      size=(24, 24))
+        self.edit_image = ctk.CTkImage(Image.open(os.path.join(IMAGES_DIR, 'edit_light.png')),
+                                       size=(24, 24))
+        self.remove_image = ctk.CTkImage(Image.open(os.path.join(IMAGES_DIR, 'remove_light.png')),
+                                         size=(24, 24))
+
+        # create widgets
         self.header = ctk.CTkLabel(self, text='Manage books',
                                    font=ctk.CTkFont(size=25, weight='bold'))
         self.search_bar = SearchBar(self, column_names, 35, 700)
         self.table = CustomTable(self, column_names, [600, 600, 600, 600])
-        self.add_button = ctk.CTkButton(self, text='add', command=self.show_form)
+
+        self.control_buttons = ctk.CTkFrame(self, fg_color='transparent')
+        self.add_button = ctk.CTkButton(self.control_buttons, corner_radius=0, text='Add',
+                                        text_color='#ffffff', font=ctk.CTkFont(size=15),
+                                        anchor='n', image=self.add_image, compound=ctk.RIGHT)
+
+        self.edit_button = ctk.CTkButton(self.control_buttons, corner_radius=0, text='Edit',
+                                         text_color='#ffffff', font=ctk.CTkFont(size=15),
+                                         anchor='n', image=self.edit_image, compound=ctk.RIGHT)
+
+        self.remove_button = ctk.CTkButton(self.control_buttons, corner_radius=0, text='Remove',
+                                           text_color='#ffffff', font=ctk.CTkFont(size=15),
+                                           anchor='n', image=self.remove_image, compound=ctk.RIGHT)
+
         self.data_form = DataForm(self, column_names, [''] * len(column_names))
-        
+
         self.show_widgets()
         self.hide_form()
-    
+
     def show_form(self, header):
         self.data_form.header.configure(text=header)
         self.data_form.place(relx=0.5, rely=0.5, anchor='center')
@@ -32,10 +58,16 @@ class BooksView(ctk.CTkFrame):
         self.header.grid_forget()
         self.search_bar.grid_forget()
         self.table.grid_forget()
-        self.add_button.grid_forget()
-    
+        self.control_buttons.grid_forget()
+        self.add_button.pack_forget()
+        self.edit_button.pack_forget()
+        self.remove_button.pack_forget()
+
     def show_widgets(self):
         self.header.grid(row=0, column=0, padx=20, pady=20)
         self.search_bar.grid(row=1, column=0, padx=20, pady=20)
         self.table.grid(row=2, column=0, padx=20, pady=20)
-        self.add_button.grid(row=3, column=0)
+        self.control_buttons.grid(row=3, column=0, pady=20)
+        self.add_button.pack(side=ctk.LEFT, padx=10)
+        self.edit_button.pack(side=ctk.LEFT, padx=10)
+        self.remove_button.pack(side=ctk.LEFT, padx=10)
