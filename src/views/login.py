0d88bdca324f37d5b45b8ba33a7ccbdfea443ecb
fluffybuzzard <699444@student.inholland.nl>
2023-06-26 18:05:38 +0200

    icon and overview

--- src/views/login.py
+++ src/views/login.py
@@ -8,55 +8,60 @@ from src.resources.config import IMAGES_DIR
 
 class LoginView(ctk.CTkFrame):
     def __init__(self, master):
-        super().__init__(master, corner_radius=5, fg_color=('gray85', 'gray10'))
+        super().__init__(master, corner_radius=5, fg_color='transparent')
 
         # load images
-        self.logo_image = ctk.CTkImage(Image.open(os.path.join(IMAGES_DIR, 'customtkinter_logo_single.png')),
-                                       size=(30, 30))
+        self.logo_image = ctk.CTkImage(Image.open(os.path.join(IMAGES_DIR, 'icon_wobg.png')),
+                                       size=(480, 180))
 
         # populate frame
-        self.logo_label = ctk.CTkLabel(self, text='  Alexandria Library', text_color='gray25',
-                                       font=ctk.CTkFont(size=35, weight='bold'), image=self.logo_image,
-                                       compound='left')
-        self.logo_label.grid(row=0, column=1, padx=20, pady=25, columnspan=2)
+        self.logo_label = ctk.CTkLabel(self, text='', text_color='gray25',
+                                       font=ctk.CTkFont(size=35, weight='bold'), image=self.logo_image, compound=ctk.LEFT)
+        self.logo_label.grid(row=0, column=1, padx=20, pady=25)
 
-        self.header = ctk.CTkLabel(self, text='Login', text_color='gray25',
+        self.container = ctk.CTkFrame(self, fg_color=('gray85', 'gray10'))
+        self.container.grid(row=1, column=1)
+
+        self.header = ctk.CTkLabel(self.container, text='Login', text_color='gray25',
                                    font=ctk.CTkFont(size=25, weight='bold'))
         self.header.grid(row=1, column=1, padx=20, pady=15, columnspan=2)
 
-        self.email_label = ctk.CTkLabel(self, text='Email', text_color='gray25', font=ctk.CTkFont(size=15),
+        self.email_label = ctk.CTkLabel(self.container, text='Email', text_color='gray25', font=ctk.CTkFont(size=15),
                                         anchor='sw')
         self.email_label.grid(row=2, column=1, sticky='nw', padx=25)
-        self.email_entry = ctk.CTkEntry(self, width=300, font=ctk.CTkFont(size=15),
+        self.email_entry = ctk.CTkEntry(self.container, width=300, font=ctk.CTkFont(size=15),
                                         placeholder_text='name.surname@example.com')
         self.email_entry.grid(row=3, column=1, padx=20, pady=(0, 15))
         # self.username_entry.after(230, self.username_entry.focus_set)  # BUGFIX - Customtkinter fix focus_set
 
-        self.password_label = ctk.CTkLabel(self, text='Password', text_color='gray25', font=ctk.CTkFont(size=15),
+        self.password_label = ctk.CTkLabel(self.container, text='Password', text_color='gray25', font=ctk.CTkFont(size=15),
                                            anchor='sw')
         self.password_label.grid(row=4, column=1, sticky='nw', padx=25)
-        self.password_entry = ctk.CTkEntry(self, width=300, show='•', placeholder_text='••••••••',
+        self.password_entry = ctk.CTkEntry(self.container, width=300, show='•', placeholder_text='••••••••',
                                            font=ctk.CTkFont(size=15))
         self.password_entry.grid(row=5, column=1, padx=20, pady=(0, 15))
 
         self.show_password = ctk.BooleanVar()
-        self.toggle_password_button = ctk.CTkCheckBox(self, text='Show', text_color='gray30',
+        self.toggle_password_button = ctk.CTkCheckBox(self.container, text='Show', text_color='gray30',
                                                       font=ctk.CTkFont(size=15), variable=self.show_password)
         self.toggle_password_button.grid(row=5, column=2, pady=(0, 15))
 
-        self.forgot_password = ctk.CTkLabel(self, text='Forgot your password?', text_color='#0074cc',
+        self.forgot_password = ctk.CTkLabel(self.container, text='Forgot your password?', text_color='#0074cc',
                                             font=ctk.CTkFont(size=15, underline=True), anchor='w', cursor='hand2')
         self.forgot_password.grid(row=6, column=1, padx=25, pady=(10, 5), columnspan=2, sticky='w')
 
-        self.guest_login_button = ctk.CTkLabel(self, text='Continue as guest', text_color='#0074cc',
+        self.guest_login_button = ctk.CTkLabel(self.container, text='Continue as guest', text_color='#0074cc',
                                             font=ctk.CTkFont(size=15, underline=True), anchor='w', cursor='hand2')
         self.guest_login_button.grid(row=7, column=1, padx=25, pady=(5, 10), columnspan=2, sticky='w')
         # TODO: add guest login access level
 
-        self.login_button = ctk.CTkButton(self, height=40, width=200, border_spacing=10, text='Login',
+        self.login_button = ctk.CTkButton(self.container, height=40, width=200, border_spacing=10, text='Login',
                                           text_color='#ffffff', font=ctk.CTkFont(size=20, weight='bold'),
                                           anchor='n')
         self.login_button.grid(row=8, column=1, columnspan=2, pady=20)
+
+    def show_messagebox(self, **kwargs):
+        CTkMessagebox(self, **kwargs)
 
     def clear_form(self):
         if self.password_entry.get():
--- src/views/overview.py
+++ src/views/overview.py
@@ -1,7 +1,11 @@
 import customtkinter as ctk
-
+from src.models.database import Database
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 class OverviewView(ctk.CTkFrame):
+
     def __init__(self, master) -> None:
         super().__init__(master, fg_color='transparent')
         self.grid_columnconfigure(0, weight=1)
@@ -9,3 +13,64 @@ class OverviewView(ctk.CTkFrame):
         self.header = ctk.CTkLabel(self, text='Library overview',
                                    font=ctk.CTkFont(size=25, weight='bold'))
         self.header.grid(row=0, column=0, padx=20, pady=20)
+
+        self.container = ctk.CTkFrame(self, corner_radius=20)
+        self.container.grid(row=1, column=0)
+
+        database = Database()
+        pie_data = database.execute_query("select genre, count(title) from books GROUP BY genre LIMIT 5;")
+        bar_data = database.execute_query("select a.title, count(b.borrowed_id) from books a INNER JOIN borrowings b on a.book_id = b.borrowed_id GROUP BY a.title;")
+        
+        # iterate through pie_data and append to lists to make it usable for plt functions
+        self.pgenres = []
+        self.pbooks = []
+        for row in pie_data:
+            self.pgenres.append(row[0])
+            self.pbooks.append(row[1])
+
+        pie_fig = Figure (figsize=(4,4), dpi = 100)
+        ax_pgenres = pie_fig.add_subplot ()
+        ax_pgenres.pie (self.pbooks, labels=self.pgenres)
+        # making it a circle
+        plt.axis ('equal')  
+        f_canvas = FigureCanvasTkAgg (pie_fig, master=self.container)
+        f_canvas.draw ()
+        f_canvas.get_tk_widget().grid(row=2,column=1, padx=20, pady=20)
+
+
+
+
+        self.genres = []
+        self.num_books = []
+
+        for row in bar_data:
+            self.genres.append (row[0])
+            self.num_books.append (row[1])
+        print (self.num_books)
+
+        bargraph_fig = Figure(figsize=(15,4), dpi=100)
+        ax_genres = bargraph_fig.add_subplot(111)
+
+        ax_genres.bar (self.genres, self.num_books)
+
+        f_canvas = FigureCanvasTkAgg (bargraph_fig, master=self.container)
+        f_canvas.draw ()
+        f_canvas.get_tk_widget().grid(row=2,column=3, padx=20, pady=20)
+
+        history_frame = ctk.CTkFrame(self)
+        history_frame.grid (row=2, column=0, padx=20, pady=20, sticky='w')
+        history = 'Long ago, in the town of Alexandria, Ms. El Abodi and Mr. Brandse birthed a library. Alexandria was small, few craved books, and the library held but a handful.Yet, time wove its magic, transforming the town into a metropolis. The library flourished, embraced eager readers, and amassed a bountiful collection.'
+        history_text = ctk.CTkLabel (history_frame, text=history, wraplength=300)
+        history_text.pack(padx=20, pady=20)
+
+
+
+
+
+
+
+
+
+
+
+        
